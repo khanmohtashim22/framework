efd1eb574e19c6d8927f83e23a9a8de2830ba3f7
Mohtashim Khan <khanmohtashim22@gmail.com>
2020-12-06 16:27:48 -0500

    added expand collapse icon

--- src/components/expand-collapse/ExpandCollapseHeading.js
+++ src/components/expand-collapse/ExpandCollapseHeading.js
@@ -2,15 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { getClasses } from '../../lib/style-utils/style-utils'
+import ExpandCollapseIcon from '../icons/expand-collapse-icon/ExpandCollapseIcon'
 
 import styles from './ExpandCollapse.module.scss'
 
 const ExpandCollapseHeading = ({ children, open }) => (
   <div className={getClasses({ styles, classes: ['heading', !open && 'borderBottom'] })}>
     <div className={styles.icon}>
-      {
-        open ? '-' : '+'
-      }
+      <ExpandCollapseIcon open={open} />
     </div>
     <div>
       {children}
---
+++ src/components/icons/expand-collapse-icon/ExpandCollapseIcon.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { getClasses } from '../../../lib/style-utils/style-utils'
+
+import styles from './ExpandCollapseIcon.module.scss'
+
+const ExpandCollapseIcon = ({ nested = false, open }) => {
+  const width = nested ? '12' : '16'
+  const translateXY = nested ? '6' : '8'
+  const y = nested ? '5' : '7'
+  const height = nested ? '12' : '16'
+  const className = getClasses({ styles, classes: [nested ? 'nested' : 'unNested'] })
+
+  return (
+    <svg
+      height={height}
+      width={width}
+      viewBox={`0 0 ${width} ${height}`}
+    >
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        {
+          open ? (
+            <g>
+              <rect x="0" y="0" width={width} height={height} />
+              <rect className={className} x="0" y={y} width={width} height="2" rx="1" />
+            </g>
+          ) : (
+            <g className={className}>
+              <rect x="0" y={y} width={width} height="2" rx="1" />
+              <rect
+                transform={`translate(${translateXY}.000000, ${translateXY}.000000) rotate(90.000000) translate(-${translateXY}.000000, -${translateXY}.000000)`} 
+                x="0"
+                y={y}
+                width={width}
+                height="2"
+                rx="1"
+              />
+            </g>
+          )
+        }
+      </g>
+    </svg>
+  )
+}
+
+ExpandCollapseIcon.propTypes = {
+  open: PropTypes.bool,
+  nested: PropTypes.bool,
+}
+
+export default ExpandCollapseIcon
